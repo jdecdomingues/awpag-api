bf911404e346de34eba1db075bfe374a28c0d8cc
Jonathan Domingues <jonathan.domingues@outlook.com>
2024-03-19 00:21:14 -0300

    Intensivão Spring Rest - Algaworks - Aula 1.3

---
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -0,0 +1,36 @@
+package com.algaworks.awpag.api.controller;
+
+import com.algaworks.awpag.domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @GetMapping("/clientes")
+
+    public List<Cliente> listar() {
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("João");
+        cliente1.setTelefone("21 99999-9999");
+        cliente1.setEmail("teste@teste.com");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("Maria");
+        cliente2.setTelefone("21 99999-9999");
+        cliente2.setEmail("teste@teste.com");
+
+        var cliente3 = new Cliente();
+        cliente2.setId(3L);
+        cliente2.setNome("Maria xpto");
+        cliente2.setTelefone("21 99999-9999");
+        cliente2.setEmail("teste@teste.com");
+
+        return Arrays.asList(cliente1, cliente2, cliente3);
+    }
+}
